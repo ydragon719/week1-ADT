81c7ac0bc0801ebef447ead0c844c0850b021d7c
ydragon719 <85536991+ydragon719@users.noreply.github.com>
2021-10-04 11:22:17 +0800

    Delete summary.c

--- summary.c
+++
@@ -1,8 +0,0 @@
-! [](https://github.com/ydragon719/week1-ADT/blob/main/images/IMG_20211003_120151.jpg)
-! [](https://github.com/ydragon719/week1-ADT/blob/main/images/IMG_20211003_120202.jpg)
-! [](https://github.com/ydragon719/week1-ADT/blob/main/images/IMG_20211003_120206.jpg)
-! [](https://github.com/ydragon719/week1-ADT/blob/main/images/IMG_20211003_120217.jpg)
-! [](https://github.com/ydragon719/week1-ADT/blob/main/images/IMG_20211003_162503.jpg)
-! [](https://github.com/ydragon719/week1-ADT/blob/main/images/IMG_20211003_162509.jpg)
-! [](https://github.com/ydragon719/week1-ADT/blob/main/images/IMG_20211003_162515.jpg)
- 本周主要进行了理论学习（图片里的）.本周主要学习了第三章的链表，栈和队列三种数据结构，课本对每种数据结构都从表和数组两中实现方法进行了阐述，具体学习内容在前边的图片里 遇到的主要问题：1.栈的具体应用中平衡符号这部分介绍的有点少，对其中部分内容不理解 2.中后缀转化部分运算符优先级感觉之前考的挺多，但是课本例子较少 3.结构体部分忘光了，写程序时很吃力 4.这三种数据结构挺难的，栈和队列都是链表操作的特殊情况，书上又从表和数组两方面介绍，我们应该侧重那个呢
